1eac21b93e605942dfa0b6c97c45d97e491b7b9b
Alisa Arnold <willowtreestar@protonmail.com>
2018-05-31 10:58:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ function shout(string){
 function sayHiToGrandma(string){
   if(string === 'I love you, Grandma.'){
     return "I love you, too.";
+  } else if (string === string.toUpperCase()){
+    return "YES INDEED!";
   }
 }
